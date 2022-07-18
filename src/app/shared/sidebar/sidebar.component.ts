919b4165c87d737403ffb8c290c81f1a3356d0ac
Eduardo Fuentes <eduardo.fuentesreyes@gmail.com>
2022-07-17 20:40:02 -0400

    Finish GifsApp

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) {}
+
+  get historial(): string[] {
+    return this.gifsService.historial;
+  }
+
+  buscar(item: string) {
+    this.gifsService.buscarGifs(item);
+  }
+}
